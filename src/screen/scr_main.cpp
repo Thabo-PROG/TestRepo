f244714368047c79a27378217f84b5afbdd88e19
Thabo Tshabalala <thabott73@gmail.com>
2021-08-16 11:21:15 +0200

    Exercise 4.1

--- src/screen/scr_main.cpp
+++ src/screen/scr_main.cpp
@@ -6,16 +6,23 @@ using namespace std;
 
 int main()
 {
-	auto myScreen = Screen{6,6};
-	myScreen.forward();
-	myScreen.set('*');
-	myScreen.down();
-	myScreen.set('*');
-	myScreen.move(3,3);
-	myScreen.set("---");
+	// Exercise 4.1
+    // Writing/Drawing letter T on the screen
+    auto NewScreen = Screen(6,6);
+    NewScreen.clear(' ');
+    NewScreen.set("******");
+    NewScreen.move(1,3);
 
-	myScreen.display();
-	cout << endl;
+    int i=0;
+    while(i<5)
+    {
+        NewScreen.down();
+        NewScreen.set('*');
+        i++;
+    }
+
+    NewScreen.display();
+    cout<<endl;
 
 	myScreen.reSize(16,16);
 	myScreen.display();
